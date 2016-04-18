88e275374cb6c3a801db993e091d8a5804b31a63
Cristian Guerrero <internethostage@example.com>
2016-04-18 16:41:27 -0700

    Add user change own password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  # attr_accessor :current_password
+  attr_accessor :current_password
 
   #validate current password when the user is updated
-  # validate :current_password_is_correct, on: :update
+  validate :current_password_is_correct, on: :update
 
   has_many :posts,    dependent: :nullify
   has_many :comments, dependent: :nullify
@@ -18,5 +18,21 @@ class User < ActiveRecord::Base
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  # Check if the inputted current password is correct when the user tries to update the password
+  def current_password_is_correct
+    # Check if the user tried changing password
+    if !password.blank?
+      #Get a reference to the user since "authenticate" method always returns false when calling on itself
+      user = User.find_by_id(id)
+
+      # Check if the user cannot be authenticated with the entered current password
+      if (user.authenticate(current_password) == false)
+        #Add an error stating that the current password is incorrect
+        errors.add(:current_password, "is incorrect.")
+      end
+    end
+  end
+
 
 end
