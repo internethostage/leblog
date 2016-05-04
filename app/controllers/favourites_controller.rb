e9463b84d000a4fa826853cb159e24590ae7754b
Cristian Guerrero <cristian@s9.cl>
2016-05-03 19:22:10 -0700

    add ajax to favourite/unfavourite posts

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,23 +1,36 @@
 class FavouritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :post, only: [:create, :destroy]
+
 
   def create
     favourite       = Favourite.new
-    post            = Post.find params[:post_id]
     favourite.post  = post
     favourite.user  = current_user
-    if favourite.save
-      redirect_to post_path(post), notice: "Added to favourites!"
-    else
-      redirect_to post_path(post), alert: "Already in your favourites"
+    respond_to do |format|
+      if favourite.save
+        format.html { redirect_to post_path(post), notice: "Added to favourites!" }
+        format.js { render }
+      else
+        format.html { redirect_to post_path(post), alert: "Already in your favourites" }
+        format.js { render js: "alert('Already in your favourites, please refresh the page!');" }
+      end
     end
   end
 
   def destroy
-    post        = Post.find params[:post_id]
     favourite   = current_user.favourites.find params[:id]
     favourite.destroy
-    redirect_to post_path(post), notice: "Post removed!"
+    respond_to do |format|
+      format.html { redirect_to post_path(post), notice: "Post removed!" }
+      format.js { render }
+    end
+  end
+
+  private
+
+  def post
+  @post ||= Post.find params[:post_id]
   end
 
 end
