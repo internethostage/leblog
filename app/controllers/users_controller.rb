7c5e95a808a17dd1fada8225a3fed08f1098057e
Cristian Guerrero <internethostage@example.com>
2016-05-15 14:04:22 -0700

    add password reset mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ before_action :find_user, only: [:edit, :update]
   def create
     @user = User.new user_params
     if @user.save
-      session[:user_id] = @user.id
+      sign_in(@user)
       redirect_to root_path, notice: "Account Created!"
     else
       render :new
